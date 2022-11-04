fc9762eacb64a5a49d0c773542d96b6b2dd0c531
shawn <40534316+anwarulislamshawn@users.noreply.github.com>
2022-11-05 05:55:07 +0600

    unit test in product controller

--- src/product/product.controller.spec.ts
+++ src/product/product.controller.spec.ts
@@ -1,20 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ResponseCreateProductDto } from './dto/response-create-product.dto';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
+const mockProductService = () => ({
+  create: jest.fn(),
+  createInventory: jest.fn(),
+  findAll: jest.fn(),
+  findAProduct: jest.fn(),
+  update: jest.fn(),
+  updateInventory: jest.fn(),
+  remove: jest.fn()
+})
 describe('ProductController', () => {
-  let controller: ProductController;
+  let productController: ProductController;
+  let productService: ProductService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-      providers: [ProductService],
+      providers: [{
+        provide: ProductService,
+        useFactory: mockProductService
+      }],
     }).compile();
 
-    controller = module.get<ProductController>(ProductController);
+    productController = module.get<ProductController>(ProductController);
+    productService = module.get<ProductService>(ProductService);
   });
+  describe('should be defined', () => {
+    it('should be defined the productController', () => {
+      expect(productController).toBeDefined();
+    });
+    it('should be defined the productService', () => {
+      expect(productService).toBeDefined();
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    describe('should call productController.create with productService.create', () => {
+      it('should call productController.create and productService.create once', async () => {
+        jest.spyOn(productService, 'create').mockResolvedValue(new ResponseCreateProductDto)
+        await productController.create(new CreateProductDto)
+        expect(productService.create).toHaveBeenCalledTimes(1);
+      });
+      it('should call productController.create and productService.create and return proper result', async () => {
+        jest.spyOn(productService, 'create').mockResolvedValue(new ResponseCreateProductDto)
+        const result = await productController.create(new CreateProductDto)
+        expect(result).toEqual(new ResponseCreateProductDto);
+      });
+    })
+  })
+
 });
